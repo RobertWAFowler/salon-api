2c13030acc748c7c142e42f956cf8f122198a807
Robert Fowler <rfowler.reg@gmail.com>
2024-10-07 16:31:08 +0200

    Add SalonServiceDetailRepository interface
    Introduced a new interface for SalonServiceDetail repository extending CrudRepository. This addition allows basic CRUD operations for SalonServiceDetail entities.

---
+++ src/main/java/com/zenhog/salonapi/repository/SalonServiceDetailRepository.java
@@ -0,0 +1,7 @@
+package com.zenhog.salonapi.repository;
+
+import com.zenhog.salonapi.entity.SalonServiceDetail;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SalonServiceDetailRepository extends CrudRepository<SalonServiceDetail, Long> {
+}
