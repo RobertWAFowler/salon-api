e3bcf63bd2821c1af2af25f7d6e7b2612f5ba70a
Robert Fowler <rfowler.reg@gmail.com>
2024-10-07 16:31:33 +0200

    Remove redundant @Repository annotation in SlotRepository
    The @Repository annotation is unnecessary because CrudRepository already implicitly provides this functionality. Cleaning up the code by removing this redundant annotation improves maintainability and clarity.

--- src/main/java/com/zenhog/salonapi/repository/SlotRepository.java
+++ src/main/java/com/zenhog/salonapi/repository/SlotRepository.java
@@ -2,8 +2,6 @@ package com.zenhog.salonapi.repository;
 
 import com.zenhog.salonapi.entity.Slot;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface SlotRepository extends CrudRepository<Slot, Long> {
 }
