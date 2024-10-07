4468715a3d2db057d2c56cdd5159d9c4eda16488
Robert Fowler <robert@magnetichq.com>
2024-10-07 21:23:36 +0200

    Rename HomeController package to `controller`
    Renamed `HomeController` from `restcontroller` package to `controller` to better reflect its responsibilities. This change improves readability and consistency within the project's package structure.

--- src/main/java/com/zenhog/salonapi/controller/HomeController.java
+++ src/main/java/com/zenhog/salonapi/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.zenhog.salonapi.restcontroller;
+package com.zenhog.salonapi.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
