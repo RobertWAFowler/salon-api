51d865652a96ade753d2c031c2852a20bc81de47
Robert Fowler <rfowler.reg@gmail.com>
2024-10-09 20:42:25 +0200

    Enable CORS for all origins
    Added a WebMvcConfigurer bean to the SalonApiApplication class to configure Cross-Origin Resource Sharing (CORS). This allows all origins to access the API, facilitating cross-domain requests without restrictions.

--- src/main/java/com/zenhog/salonapi/SalonApiApplication.java
+++ src/main/java/com/zenhog/salonapi/SalonApiApplication.java
@@ -2,6 +2,9 @@ package com.zenhog.salonapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SalonApiApplication {
@@ -10,4 +13,15 @@ public class SalonApiApplication {
         SpringApplication.run(SalonApiApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer configure() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry corsRegistry) {
+                corsRegistry.addMapping("/**").allowedOrigins("*");
+            }
+
+        };
+    }
 }
